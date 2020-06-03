6bb7d46879c29c4cbcf3c41b1379ce5829d2d485
Josh Hart <joshua.hart@protonmail.com>
2020-06-03 20:50:33 +0100

    commented script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 const calculator = {
     _display: document.getElementById('display'),  //this will be the property that will be updated as buttons are pushed
     _buttons: {
-        _numbers: [],
-        _operators: [],
+        _numbers: [],  //will fill this in using a class
+        _operators: [], //will fill in using a class
         _clear: {},
         _equals: {}
     },
@@ -39,9 +39,9 @@ calculator.display = 0;
 
 //create button objects and insert into calculator
 
-const numberButtonID = ['zero','one','two','three','four','five','six','seven','eight','nine','decimal']
+const numberButtonID = ['zero','one','two','three','four','five','six','seven','eight','nine','decimal']  //these values match the IDs of my numerical buttons in HTML
 
-class NumberButton {
+class NumberButton { //class to produce button objects from
     constructor(name) {
         this.name = name;
         this.source = document.getElementById(name);
@@ -53,7 +53,10 @@ class NumberButton {
     }
 }
 
-numberButtonID.forEach( button => calculator._buttons._numbers.push(new NumberButton(button)) );
+numberButtonID.forEach( button => calculator._buttons._numbers.push(new NumberButton(button)) );  //create an instance of the button using 
 
+// Maybe create a superclass for all buttons with the basic constructor, then create subclasses for numbers, operators etc.
+// Operators will have unique functions but will share similar behaviour with respect to the display
+// clear and equals will be their own buttons as they're unlike the others.
 
-
+//
