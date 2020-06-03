f05ceb08d0817ca7245fa47fdd3808917a78ea29
Josh Hart <joshua.hart@protonmail.com>
2020-06-03 13:50:10 +0100

    fixed JS variable declarations

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //first, lets make sure the calculator outputs '0' in the beginning
 
-let output = 0; //define a variable which will be updated as buttons are pressed.
-const display = document.getElementById('display-container').innerHTML; //this will make it quicker to reassign each time
-display = output;
+let output = '0'; //define a variable which will be updated as buttons are pressed.
+let display = document.getElementById('display-container'); //this will make it quicker to reassign each time
+display.innerHTML = output;
 
 
 
