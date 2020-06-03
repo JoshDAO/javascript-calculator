de3e83ef26e95293b8f0224b34d5990b3c2c94db
Josh Hart <joshua.hart@protonmail.com>
2020-06-03 14:05:23 +0100

    styled display box with positioning, font size and alignment

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //first, lets make sure the calculator outputs '0' in the beginning
 
 let output = '0'; //define a variable which will be updated as buttons are pressed.
-let display = document.getElementById('display-container'); //this will make it quicker to reassign each time
+let display = document.getElementById('display'); //this will make it quicker to reassign each time
 display.innerHTML = output;
 
 
