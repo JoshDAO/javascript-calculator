a92d89af620e023baa6c4826223a736c75b15062
Josh Hart <joshua.hart@protonmail.com>
2020-06-03 14:26:50 +0100

    refactored script.js to an OOP format. Created calculator object and tested for first error message

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 //first, lets make sure the calculator outputs '0' in the beginning
 
-let output = '0'; //define a variable which will be updated as buttons are pressed.
-let display = document.getElementById('display'); //this will make it quicker to reassign each time
-display.innerHTML = output;
+const calculator = {
+    _display: document.getElementById('display'),
+    get display() {
+        return this._display;
+    },
 
+    set display(newOutput) {
+        if (newOutput < 1e16 && newOutput > -1e16){
+            this._display.innerHTML = newOutput;
+        } else {
+            this._display.innerHTML = 'Error';
+        }
+    }
+}
 
-
+calculator.display = 400000000000000000000000000;
