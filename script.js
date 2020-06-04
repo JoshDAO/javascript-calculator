f334203b87c03737a77db49ae8ecaa4397a038f4
Josh Hart <joshua.hart@protonmail.com>
2020-06-04 14:22:07 +0100

    Number buttons are added and displaying in display correctly

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
+
 //first, create calculator object
 
 const calculator = {
     _display: document.getElementById('display'),  //this will be the property that will be updated as buttons are pushed
     _buttons: {
-        _numbers: [],  //will fill this in using a class
-        _operators: [], //will fill in using a class
+        _numbers: [ zero, one, two, three, four, five, six, seven, eight, nine, decimal],  
+        _operators: ['add','subtract','multiply','divide'], 
         _clear: {},
         _equals: {}
     },
 
     get display() {  // basic getter function
-        return this._display;
+        return this._display.innerHTML;
     },
 
     set display(newOutput) {        // setter function with built in Error capability for numbers too large or small. (may adjust this later)
@@ -20,46 +21,53 @@ const calculator = {
             this._display.innerHTML = 'Error';
         }
     },
-    addButtons(type) { 
-        let newButton = {
-          value,
-          source,  
-        };
-        this._buttons.push(newButton);
-        return newButton;
-
+    get numberButtons(){
+        return this._buttons._numbers;
     },
-    buttonPress(obj) {
-        this.display(button);
+    numberButtonDisplay(event) {
+        calculator._display.innerHTML += event.target.value;
+        console.log('logged')
     },
+    
     resetDisplay() {
-        this._display.innerHTML = 0;
+        this._display.innerHTML = 0; 
     }
 }
 //make sure the calculator outputs '0' in the beginning
 
-calculator.resetDisplay();
+document.onload = calculator.resetDisplay();
+calculator.numberButtons.forEach( button => button.onclick = calculator.numberButtonDisplay)
+
+//fill number buttons array
+let numberList = document.querySelectorAll('.js-number');
+calculator.numberButtons = numberList;
+calculator.numberButtons.forEach( button => button.value = button.innerHTML);
+calculator.numberButtons.forEach( button => button.onclick = calculator.numberButtonDisplay);
+
+
+
 
 //create button objects and insert into calculator
 
-const numberButtonID = ['zero','one','two','three','four','five','six','seven','eight','nine','decimal']  //these values match the IDs of my numerical buttons in HTML
-const numberButtonValue = [0,1,2,3,4,5,6,7,8,9, '.']  // values assigned to the respective buttons
-class NumberButton { //class to produce button objects from
-    constructor(name, value) {
-        this.name = name;
-        this.source = document.getElementById(name);
-        this.value = value;
-    }
-    displayNumber(){
-        this.source.onclick( () => {
-            buttonPress(this)
-        })
+
+const numberButtonFactory = (name, value) => {
+    return {
+         name,
+         value,
+         source: document.getElementById(name)
     }
 }
+
+        
+//     }
+// }
 // iterate through arrays and use data to form button instances
-numberButtonID.forEach( (button, index) => calculator._buttons._numbers.push(new NumberButton(button, numberButtonValue[index])) );
+//numberButtonID.forEach( (button, index) => calculator._buttons._numbers.push(new NumberButton(button, numberButtonValue[index])) );
 
-
+// calculator.numberButtons.forEach( button => button.source.onclick = calculator.numberButtonPress)
+// let one = document.getElementById('one');
+// calculator.numberButtons.push(one)
+// one.value = 1;
 
 // Maybe create a superclass for all buttons with the basic constructor, then create subclasses for numbers, operators etc.
 // Operators will have unique functions but will share similar behaviour with respect to the display
