132b83ae83c03ec45caaee0a18119d8e77b2213f
Josh Hart <joshua.hart@protonmail.com>
2020-06-05 21:32:39 +0100

    fixed issue with multiply/divide by negative number

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ const calculator = {
         _operators: [add, subtract, multiply, divide, clear, equals],                      // as will these
     },
     _calculatedDisplay: document.getElementById('current-result'), // will dynamically show current answer
-    _calculation: "",
-    allowDecimal: true,
+    _calculation: "", //this will have a similar value to the current display, but using the language recognised operators instead of the symbols on the operator keys. This will be what will be evaluated.
+    allowDecimal: true, // validation to ensure no more than one decimal dot per number
 
     get display() {  // basic getter function
         return this._display.innerHTML;
@@ -32,13 +32,12 @@ const calculator = {
         if (!calculator.allowDecimal && event.target === decimal){ // check to see if there is already a decimal point in the dislayed number
             return;  //dont accept another decimal input
         };
-        if (event.target === decimal) {
+        if (event.target === decimal) {   // turns variable false after decimal point is used.
             calculator.allowDecimal = false;
         }
 
         if (calculator.display === '0' && event.target !== decimal) { // if inputting a digit when the display reads 0
             calculator._display.innerHTML = event.target.value;  //replace current display instead of concatenating to it
-            calculator._startNewDisplay = false;
         } else {
             calculator._display.innerHTML += event.target.value; // else concatenate
         
@@ -50,38 +49,69 @@ const calculator = {
         }
     },
     operatorOnPress(event) {
-        calculator.allowDecimal = true;
-        calculator._display.innerHTML += event.target.value;
-        calculator._calculation += event.target.value
+        calculator.allowDecimal = true; // allows decimal point to be used again as start of a new number
+        calculator._display.innerHTML += event.target.innerHTML; //add operator to display
+        calculator._calculation += event.target.value  //add operator to calculation
     },
     resetDisplay(event) { //used for clear button
         calculator._display.innerHTML = '0'
         calculator._calculatedDisplay.innerHTML = "";
         calculator._calculation = '';
     },
+    equalsOnPress(event){
+        calculator._display.innerHTML = calculator._calculatedDisplay.innerHTML; // make the calculated number appear big
+        calculator._calculatedDisplay.innerHTML = ""; //remove the small display until further calculation
+    },
     calculateAddition(expression) {
         const splitExpression = expression.split('+');
+        splitExpression.forEach( (element, index) => {
+            if (element.endsWith('/') || element.endsWith('*')){
+                console.log(element);
+                //element += splitExpression[index + 1];
+               // splitExpression[index + 1] /= -1;
+            }
+        });
+        console.log(splitExpression);
         const numberExpression = splitExpression.map( element => this.calculateSubtraction(element));
-        console.log(numberExpression);
+       // console.log(numberExpression);
         const result = numberExpression.reduce( (accumulator, currentValue) => {return accumulator + currentValue}, 0);
         return result;
     },
     calculateSubtraction(expression) {
-        const splitExpression = expression.split('-');
+        let splitExpression = expression.split('-');
+        for (let i = 0; i < splitExpression.length; i++){ // this is to fix a bug in the calculation where there is a double operator. EG. '5/-4'
+            if (splitExpression[i].endsWith('/') || splitExpression[i].endsWith('*')) {  
+                console.log(splitExpression[i].endsWith('/'))
+                splitExpression[i] = '-' + splitExpression[i].toString() + splitExpression[i+1].toString();
+                splitExpression.splice(i+1, 1)
+            }
+        }
+        
+        console.log(splitExpression);
         const numberExpression = splitExpression.map( element =>  this.calculateMultiplication(element));
+       // console.log(numberExpression);
 
         const result = numberExpression.slice(1).reduce( (accumulator, currentValue) => { return accumulator - currentValue}, numberExpression[0] ); // we need to use the first element as initialValue and only subtract subsequent
         return result;
     },
     calculateMultiplication(expression) {
         const splitExpression = expression.split('*');
+        console.log(splitExpression);
+        // splitExpression.forEach( (element, index) => {
+        //     if (element.endsWith('/') || element.endsWith('*')){
+        //         element += splitExpression[index + 1];
+        //         splitExpression[index + 1] /= -1;
+        //     }
+        // });
+
         const numberExpression = splitExpression.map( element => this.calculateDivision(element));
-        //console.log(numberExpression);
         const result = numberExpression.reduce( (accumulator, currentValue) => { return accumulator * currentValue}, 1 ); // use 1 as a constant to multiply everything by
         return result;
     },
     calculateDivision(expression) {
         const splitExpression = expression.split('/');
+        console.log(splitExpression);
+
         const numberExpression = splitExpression.map( element => parseFloat(element));
         //console.log(numberExpression);
         const result = numberExpression.slice(1).reduce( (accumulator, currentValue) => { return accumulator / currentValue}, numberExpression[0] );
@@ -94,13 +124,11 @@ const calculator = {
 
 document.onload = calculator.resetDisplay();
 
+// ---- LINK ELEMENTS AND ASSIGN APPROPRIATE PROPERTIES ----
+
 //fill number buttons array
 let numberList = document.querySelectorAll('.js-number');
 calculator.numberButtons = numberList;
-calculator.numberButtons.forEach( button => {
-    button.value = button.innerHTML;
-    button.onclick = calculator.numberOnPress;
-});
 
 
 
@@ -115,30 +143,18 @@ calculator.operatorButtons[3].value = '/';
 
 //-----BUTTON EVENT LISTENERS-----
 
+// add number button functionality
+    calculator.numberButtons.forEach( button => {
+        button.value = button.innerHTML;
+        button.onclick = calculator.numberOnPress;
+    });
+
 //add clear button functionality
 clear.onclick = calculator.resetDisplay
 
-//add general oprator button functionality
+//add general operator button functionality
 calculator.operatorButtons.slice(0,4).forEach( operatorButton => operatorButton.onclick = calculator.operatorOnPress); //add event listener to each of 4 operator buttons
 
+//add equals functonality
+equals.onclick = calculator.equalsOnPress;
 
-equals.onclick = calculator.calculate;
-
-
-
-
-// Maybe create a superclass for all buttons with the basic constructor, then create subclasses for numbers, operators etc.
-// Operators will have unique functions but will share similar behaviour with respect to the display
-// clear and equals will be their own buttons as they're unlike the others.
-
-// Things to do:
-// - get numbers keys to appear in the display. Treat them like strings and concatenate them into the calculator.display property. Decimal is classed as a number.
-// - create a function which tests to ensure that a number cannot begin with >1 zero and decimal point cannot appear twice or more.
-// - create memory variable which stores current value of calculations and will be returned upon hitting equals.
-//
-//
-//
-//
-//
-//
-//
