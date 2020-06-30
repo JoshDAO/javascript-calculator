c597759d68d47e14eba390be0c5929c481655728
Josh Hart <joshua.hart@protonmail.com>
2020-06-30 16:38:36 +0100

    fixed bug with exponential notation and commented

--- script.js
+++ script.js
@@ -78,13 +78,30 @@ const calculator = {
         calculator._calculation = calculator.display //replace the calculation with the result of the calculation to prevent issues when operating on this value
         calculator.equalsReset = true;
     },
+
+    //Below are the main calculation methods. The equation (a string) will be passed as an argument into calculateAddition.
+    //calculateAddition will split the string into sub-equations at each '+' operator.
+    //Those sub-equations are passed into similar methods that handle subtraction, then multiplication, then division.
+    //The methods are in this order so that division is evaluated first, then the evaluated sub-equation is passed back to the multiplication method and further evaluated.
+    //Lastly, once multiplication and division have been evaluated, it is passed back to calculateSubtraction, then calculateAddition - maintaining operator precedence rules.
+
+
+
     calculateAddition(expression) {
-        if (expression.startsWith('-')) {
-            expression = "1*-1* " +expression.substring(1);
+        if (expression.startsWith('-')) { //this if statement deals with first number being a negative
+            expression = "1*-1* " +expression.substring(1); // refactor equation so negative symbol is not at start and the split method below will work as intended.
         }
-        const splitExpression = expression.split('+');
-        const numberExpression = splitExpression.map( element => this.calculateSubtraction(element));
-        const result = numberExpression.reduce( (accumulator, currentValue) => {return accumulator + currentValue}, 0);
+        const splitExpression = expression.split('+'); //split equation at every instance of plus sign
+
+        for (let i = 0; i < splitExpression.length; i++) {  // This if statement is to fix a bug when dealing with exponential notation
+            if (splitExpression[i].endsWith('E')) {  // a number '1E+30' was being split into ['1E', '30']
+                splitExpression[i] = splitExpression[i] + splitExpression[i+1]; //so this line pushes those two elements back into the same element, restoring the number
+                splitExpression.splice(i+1, 1); // remove the now redundant element
+                i--; 
+            }
+        }
+        const numberExpression = splitExpression.map( element => this.calculateSubtraction(element)); //numberExpression represents the array filled with all of the returned and evaluated sub-equations
+        const result = numberExpression.reduce( (accumulator, currentValue) => {return accumulator + currentValue}, 0); //this method is calculating ADDITION, so reduce array to find the sum of its parts.
         return result;
     },
     calculateSubtraction(expression) {
@@ -99,8 +116,8 @@ const calculator = {
             }
         }
       
-        const numberExpression = splitExpression.map( element =>  this.calculateMultiplication(element));
-        const result = numberExpression.slice(1).reduce( (accumulator, currentValue) => { return accumulator - currentValue}, numberExpression[0] ); // we need to use the first element as initialValue and only subtract subsequent
+        const numberExpression = splitExpression.map( element =>  this.calculateMultiplication(element)); //numberExpression is the array containing evaluated sub-equations from multiplication and division.
+        const result = numberExpression.slice(1).reduce( (accumulator, currentValue) => { return accumulator - currentValue}, numberExpression[0] ); // we need to use the first element as initialValue and only subtract subsequent elements.
         return result;
     },
     calculateMultiplication(expression) {
@@ -119,10 +136,10 @@ const calculator = {
     calculateDivision(expression) {
         const splitExpression = expression.split('/');
         const numberExpression = splitExpression.map( element => parseFloat(element));
-        const result = numberExpression.slice(1).reduce( (accumulator, currentValue) => { return accumulator / currentValue}, numberExpression[0] );
+        const result = numberExpression.slice(1).reduce( (accumulator, currentValue) => { return accumulator / currentValue}, numberExpression[0] ); // again, use first element as initial value.
         return result;
     },
-    subsequentOperatorTest(){
+    subsequentOperatorTest(){ 
         if (this.calculation.endsWith('-') || 
         this.calculation.endsWith('+') ||
         this.calculation.endsWith('*') ||
